0b0cb9d20192ebff3a8865050ee68f61421e92eb
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2017-06-02 16:54:24 +0700

    Implement Lec1's Algo

---
+++ Lec1_peakFinding/peakFinding.cpp
@@ -0,0 +1,91 @@
+#include "peakFinding.h"
+
+using namespace std;
+
+int peak_1D(int* arr, int left, int right){
+  if (left != right){
+    int middle = (left + right) / 2;
+    if (arr[middle] < arr[middle-1]){
+      return peak_1D(arr, left, middle-1);
+    }
+    else if (arr[middle] < arr[middle+1]){
+      return peak_1D(arr, middle+1, right);
+    }
+    else {
+      return middle;
+    }
+  }
+  else return left;
+}
+
+int max_pos(int* arr, int length){
+  int max_pos = 0;
+  for (int i = 1; i < length; i++){
+    if (arr[i] > arr[max_pos]){
+      max_pos = i;
+    }
+  }
+  return max_pos;
+}
+
+pos_2D peak_2D(int** mat, int above, int bottom, int cols){
+  pos_2D peak;
+  int middle_row = (above + bottom) / 2;
+  int max_pos = max(*mat[middle_row], cols);
+  if (above != bottom){
+    if (mat[middle_row][max_pos] < mat[middle_row-1][max_pos]){
+      return peak_2D(mat, above, middle_row-1, cols);
+    }
+    else if (mat[middle_row][max_pos] < mat[middle_row+1][max_pos]){
+      return peak_2D(mat, middle_row+1, bottom, cols);
+    }
+    else {
+      peak.x = middle_row+1;
+      peak.y = max_pos+1;
+      return peak;
+    }
+  }
+  else {
+    peak.x = above+1;
+    peak.y = max_pos+1;
+    return peak;
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  /*
+  int n;
+  cout << "Input the length of arr: ";
+  cin >> n;
+  int* arr = new int[n];
+  cout << "Input the arr: ";
+  for (int i = 0; i < n; i++){
+    cin >> arr[i];
+  }
+  int peak = peak_1D(arr, 0, n-1);
+  cout << "The position of peak of arr is " << peak+1 << endl;
+  */
+
+  int rows;
+  int cols;
+  cout << "Input the number of row of the matrix: ";
+  cin >> rows;
+  cout << "Input the number of column of the matrix: ";
+  cin >> cols;
+
+  int** mat = new int*[rows];
+  for (int i = 0; i < rows; i++){
+    mat[i] = new int[cols];
+  }
+  cout << "Input the matrix:" << endl;
+  for (int i = 0; i < rows; i++){
+    cout << "Input the row " << i+1 << endl;
+    for (int j = 0; j < cols; j++){
+      cin >> mat[i][j];
+    }
+  }
+
+  pos_2D peak = peak_2D(mat, 0, rows-1, cols);
+  cout << "The position of peak of the matrix is (" << peak.x << "," << peak.y << ")" << endl;
+  return 0;
+}
---
+++ Lec1_peakFinding/peakFinding.h
@@ -0,0 +1,29 @@
+#ifndef peakFinding_H
+#define peakFinding_H
+
+#include <iostream>
+
+struct pos_2D{
+  int x;
+  int y;
+};
+//  arr is an integer arrray
+//  left is the begin of arr
+//  right is the end of arr
+//  if arr has n elements then left = 0 and right = n-1
+//  function will return position of the peak
+int peak_1D(int* arr, int left, int right);
+
+//  arr is an integer array
+//  length is the length of arr
+//  function will return position of maximum
+int max(int* arr, int length);
+
+//  mat is an integer matrix
+//  above is the begining row of the matrix
+//  bottom is the ending row of the matrix
+//  cols is the number column of the matrix
+//  function will return position of the peak
+pos_2D peak_2D(int** mat, int above, int bottom, int cols);
+
+#endif
