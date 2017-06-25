a6f9503e6ba8e0cf683cfdbd9fd783e103aa7a45
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2017-06-25 21:20:40 +0700

    Sorting Algorithms

--- Lec3/Insertion_sort.cpp
+++ Lec3/Insertion_sort.cpp
@@ -50,5 +50,6 @@ int main(int argc, char const *argv[]) {
   // Printing running time of algorithm
   double runningTime = ((double)(end - start) / CLOCKS_PER_SEC) * 1000;
   cout << "Running time of the Algorithm is " << runningTime << " ms" << endl;
+
   return 0;
 }
