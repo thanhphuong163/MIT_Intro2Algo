39046f6e38951a99daebd38123af552024e48be3
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2017-08-16 21:47:17 +0700

    Update more exercises

--- Exercises/CountPairsSameSum.cpp
+++ Exercises/CountPairsSameSum.cpp
@@ -37,6 +37,42 @@ int countPairs(vector<int> v1, vector<int> v2, int n) {
   return count;
 }
 
+bool compare(int prev, int next) {
+  return prev > next;
+}
+
+int countPairs_sol(vector<int> v1, vector<int> v2, int n) {
+  int count = 0;
+  sort(v1.begin(), v1.begin() + v1.size());
+  sort(v2.begin(), v2.begin() + v2.size(), compare);
+  for (int e : v1) {
+    cout << e << " ";
+  }
+  cout << endl;
+  for (int e : v2) {
+    cout << e << " ";
+  }
+  cout << endl;
+  int i = 0;
+  int j = 0;
+  int size1 = v1.size();
+  int size2 = v2.size();
+  while (i < size1 && j < size2) {
+    if (v1[i] + v2[j] == n) {
+      count++;
+      i++;
+      j++;
+    }
+    else if (v1[i] + v2[j] < n) {
+      i++;
+    }
+    else {
+      j++;
+    }
+  }
+  return count;
+}
+
 int main(int argc, char const *argv[]) {
   int arr1[7] = {4,3,5,7,11,2,1};
   int arr2[7] = {2,3,4,5,6,8,12};
@@ -45,7 +81,7 @@ int main(int argc, char const *argv[]) {
     v1.push_back(arr1[i]);
     v2.push_back(arr2[i]);
   }
-  int count = countPairs(v1,v2,9);
+  int count = countPairs_sol(v1,v2,9);
   cout << count << endl;
   return 0;
 }
